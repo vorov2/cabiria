369c129041518876bf34d592774c4b58d8ec3caf
Василий Воронков <nic-vorov2@yandex.ru>
2024-11-11 09:53:06 +0300

    -

--- main3.lua
+++ main3.lua
@@ -1,5 +1,5 @@
 -- $Name: Бездна света$
--- $Version: 1.2$
+-- $Version: 1.3$
 -- $Author: Василий Воронков$
 -- $Info: Текстовая игра для платформы INSTEAD (https://instead.hugeping.ru)$
 dofile "lib/es.lua"
